1112081f37629bafc65ebaa1dac4ab4527690c5c
Thomas Uster <thomasuster@gmail.com>
2015-06-23 22:18:56 -0700

    delay on android nexus 5

--- Source/Main.hx
+++ Source/Main.hx
@@ -11,55 +11,15 @@ import nme.Assets;
 
 class Main extends Sprite {
 
-    var flag:Bool;
 	var channel:SoundChannel;
-    var a:Accelerometer;
-    var i:Int;
-    var app:AndroidApplovin;
 
 	public function new () {
 		
 		super ();
-//        toggleSound();
-        stage.addEventListener(MouseEvent.CLICK, onEnter);
-//        a = new Accelerometer();
-//        a.setRequestedUpdateInterval(1);
-//        a.addEventListener(AccelerometerEvent.UPDATE, onAccel);
 
-        app = new AndroidApplovin();
-        app.init();
-        app.preload();
-	}
-
-    function asd():Void {
-    }
-
-        var lastX:Float;
-        var lastY:Float;
-    function onAccel(e:AccelerometerEvent):Void {
-//        i++;
-//        if(i > 10)
-//            a.removeEventListener(AccelerometerEvent.UPDATE, onAccel);
-//        trace(e.accelerationX + " " + e.accelerationY);
-    }
-
-    function onEnter(e:Dynamic):Void {
-        trace(app.isReady());
-        app.show();
-//        flag = !flag;
-//        channel.stop();
-//        toggleSound();
-    }
-
-    function toggleSound():Void {
-        var name:String = 'melody_music';
-        if(flag)
-            name = 'decorate_music';
-        var sound:Sound = Assets.getMusic('assets/$name.mp3', false);
+        var sound:Sound = Assets.getMusic('assets/Party_Gu-Jeremy_S-8250_hifi.ogg', false);
         var soundTransform = new SoundTransform();
         soundTransform.volume = 0.5;
         channel = sound.play(0, Math.ceil(0xFFFFFF), soundTransform);
-    }
-
-	
+	}
 }
