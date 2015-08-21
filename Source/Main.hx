355f925ffc317342b365ecd4f85726791348f687
Thomas Uster <thomasuster@gmail.com>
2015-08-21 11:04:08 -0700

    Likely optional method getMainFileMethod compiling, downloader client fix for thrown error.

--- Source/Main.hx
+++ Source/Main.hx
@@ -7,24 +7,29 @@ import flash.media.SoundChannel;
 import flash.media.SoundTransform;
 import flash.media.Sound;
 import nme.Assets;
+import extension.androidExpansion.AndroidExpansion;
 
 class Main extends Sprite {
 
-	var channel:SoundChannel;
-
 	public function new () {
-		
 		super ();
-
-//        var sound:Sound = Assets.getMusic('assets/Party_Gu-Jeremy_S-8250_hifi.ogg', false);
-//        var soundTransform = new SoundTransform();
-//        soundTransform.volume = 0.5;
-//        channel = sound.play(0, 10000, soundTransform);
-        Reflect.callMethod(this, someMethod, []);
-
+        addEventListener(Event.ADDED_TO_STAGE, onAdded);
 	}
 
-    function someMethod():Void {
-        trace("hello world!");
+    public function onAdded(event:Event):Void {
+        removeEventListener(Event.ADDED_TO_STAGE, onAdded);
+        Sys.println("ASDASDASD");
+        trace("!!!!!!!!!!!!!!!!!");
+        var delivered:Bool = AndroidExpansion.expansionFilesDelivered();
+        trace('expansion.expansionFilesDelivered() ' + delivered);
+        if(!delivered) {
+            trace('expansion.startDownloadServiceIfRequired-started');
+            var result:Int = AndroidExpansion.startDownloadServiceIfRequired();
+            trace('result = ' + result);
+            trace('expansion.startDownloadServiceIfRequired-ended');
+        }
+        else {
+            trace('main file? = ' + AndroidExpansion.getMainFile());
+        }
     }
 }
